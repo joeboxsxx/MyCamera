63320822c4222383fc9fcab35193d8c6d6b77b65
joeboxsxx <joeboxsxx@ezweb.ne.jp>
2023-03-05 11:54:57 +0900

    Initial Commit

---
+++ MyCamera/MyCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCameraApp.swift
+//  MyCamera
+//
+//  Created by 戸上健太 on 2023/03/05.
+//
+
+import SwiftUI
+
+@main
+struct MyCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
