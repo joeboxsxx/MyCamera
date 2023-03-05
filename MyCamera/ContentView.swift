3e94e00ac6ed0854b371f92f5bfd80948cb845a5
joeboxsxx <joeboxsxx@ezweb.ne.jp>
2023-03-05 17:59:46 +0900

    Placement and implementation of the camera launch button

--- MyCamera/ContentView.swift
+++ MyCamera/ContentView.swift
@@ -8,14 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    // 撮影した写真を保持する状態変数
+    @State var captureImage: UIImage? = nil
+    // 撮影画面(sheet)の開閉状態を管理
+    @State var isShowSheet = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+            
+            if let captureImage {
+                Image(uiImage: captureImage)
+                    .resizable()
+                    // アスペクト比(縦横比)を維持して画面に収める
+                    .scaledToFit()
+            }
+            
+            Spacer()
+            
+            Button {
+                // カメラが利用可能かチェック
+                if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                    print("カメラは利用できます")
+                    // isShowSheetをtrue
+                    isShowSheet.toggle()
+                } else {
+                    print("カメラは利用できません")
+                }
+            } label: {
+                Text("カメラを起動する")
+                    // 横幅いっぱい
+                    .frame(maxWidth: .infinity)
+                    // 高さ50ポイントを指定
+                    .frame(height: 50)
+                    // 文字列をセンタリング指定
+                    .multilineTextAlignment(.center)
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+            }
+            .padding()
+            // isPresentedで指定した状態変数がtrueのとき実行
+            .sheet(isPresented: $isShowSheet) {
+                // UIImagePickerController(写真撮影)を表示
+                ImagePickerView(isShowSheet: $isShowSheet, captureImage: $captureImage)
+            }
         }
-        .padding()
     }
 }
 
