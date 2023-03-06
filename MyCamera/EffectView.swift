642dcb0558be53f8e19ee7284ee1409d85abff30
joeboxsxx <joeboxsxx@ezweb.ne.jp>
2023-03-06 16:22:48 +0900

    Implementing effect buttons and close buttons

--- MyCamera/EffectView.swift
+++ MyCamera/EffectView.swift
@@ -29,6 +29,39 @@ struct EffectView: View {
             
             // 「エフェクト」ボタン
             Button {
+                // フィルタ名を指定
+                let filterName = "CIPhotoEffectMono"
+                // 元々の画像の回転角度を取得
+                let rotate = captureImage.imageOrientation
+                // UIImage形式の画像をCIImage形式に変換
+                let inputImage = CIImage(image: captureImage)
+                
+                // フィルタの種別を引数で指定された種類を指定してCIFilterのインスタンスを取得
+                guard let effectFilter = CIFilter(name: filterName) else {
+                    return
+                }
+                
+                // フィルタ加工のパラメータを初期化
+                effectFilter.setDefaults()
+                // インスタンスにフィルタ加工する元画像を設定
+                effectFilter.setValue(inputImage, forKey: kCIInputImageKey)
+                // フィルタ加工を行う情報を生成
+                guard let outputImage = effectFilter.outputImage else {
+                    return
+                }
+                
+                // CIContextのインスタンスを取得
+                let ciContext = CIContext(options: nil)
+                // フィルタ加工後の画像をCIContext上に描画し、結果をcgImageとしてCFImage形式の画像を取得
+                guard let cgImage = ciContext.createCGImage(outputImage, from: outputImage.extent) else {
+                    return
+                }
+                // フィルタ加工後の画像をCIImage形式からUIImage形式に変更。その際に回転角度を指定。
+                showImage = UIImage(
+                    cgImage: cgImage,
+                    scale: 1.0,
+                    orientation: rotate
+                )
                 
             } label: {
                 Text("エフェクト")
@@ -56,7 +89,8 @@ struct EffectView: View {
             
             // 「閉じる」ボタン
             Button {
-                
+                // エフェクト編集画面を閉じる
+                isShowSheet.toggle()
             } label: {
                 Text("閉じる")
                     .frame(maxWidth: .infinity)
